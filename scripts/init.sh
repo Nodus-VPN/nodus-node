d0b69d89dd2d770c7021bcf2be6d61d312df5b8a
mm <maksmikhilov01@gmail.com>
2024-10-07 22:11:46 +0500

    dev

--- scripts/init.sh
+++ scripts/init.sh
@@ -68,19 +68,3 @@ else
 fi
 
 docker compose -f docker/docker-compose.yml up --build
-
-
-#docker network create net
-#docker run -d \
-#  --name=wg-easy \
-#  --network=net \
-#  -e WG_HOST=$(wget -q -4 -O- http://icanhazip.com) \
-#  -v ~/.wg-easy:/etc/wireguard \
-#  -p 51820:51820/udp \
-#  -p 51821:51821/tcp \
-#  --cap-add=NET_ADMIN \
-#  --cap-add=SYS_MODULE \
-#  --sysctl="net.ipv4.conf.all.src_valid_mark=1" \
-#  --sysctl="net.ipv4.ip_forward=1" \
-#  --restart unless-stopped \
-#  ghcr.io/wg-easy/wg-easy
