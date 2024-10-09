5152148f42676019ff5e8fa756184cf4440cd27d
mm <maksmikhilov01@gmail.com>
2024-10-10 00:43:39 +0500

    dev

--- internal/model/model.py
+++ internal/model/model.py
@@ -8,8 +8,6 @@ class NodeClient:
     wg_id: str
     ovpn_id: str
     address: str
-    traffic: int
-    latest_handshake: datetime
     created_at: datetime
     updated_at: datetime
 
@@ -20,8 +18,6 @@ class NodeClient:
             wg_id=row.wg_id,
             ovpn_id=row.ovpn_id,
             address=row.address,
-            traffic=row.traffic,
-            latest_handshake=row.latest_handshake,
             created_at=row.created_at,
             updated_at=row.updated_at
         ) for row in rows]
